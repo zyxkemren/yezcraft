13c8da3fac453379727e89cd2f5e31dc9a60233a
ZYX <zyxemail.24@gmail.com>
2024-01-20 14:37:27 +0700

    sddsfdf

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('/'));
+app.use(express.static("/"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
 app.use(bodyParser.json());
 
 // Menangani permintaan POST ke /webhook/trakteer
-app.post('/webhook/trakteer', (req, res) => {
-  console.log('Received POST request to /webhook/trakteer');
-  console.log('Request Body:', req.body);
-  res.status(200).send({ status: 'Success' });
+app.post("/webhook/trakteer", (req, res) => {
+  console.log("Received POST request to /webhook/trakteer");
+  console.log("Request Body:", req.body);
+  res.status(200).send({ status: "Success" });
 });
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname + '/404.html');
+  res.status(404).send("404 not found !!!");
 });
 
 app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
