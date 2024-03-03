9783612d1145dfc239766a280b058862d0804947
ZYX <zyxemail.24@gmail.com>
2024-03-03 10:43:35 +0700

    asd

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post("/webhook/trakteer", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.status(404).send("404 not found !!!");
+  res.status(404).sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
