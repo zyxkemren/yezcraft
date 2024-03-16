86bae3fcdcb935e9acb5ead4398d27a8ceda2254
ZYX <zyxemail.24@gmail.com>
2024-03-16 11:33:42 +0700

    DB

--- server/processPayment.js
+++ server/processPayment.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
 });
 
 const processPayment = async (req, res) => {
