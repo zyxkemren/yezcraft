9b0106de9bbf0743d2fbbdc8399b734dd646a647
ZYX <zyxemail.24@gmail.com>
2024-03-16 12:04:28 +0700

    DB NAME

--- server/getUUID.js
+++ server/getUUID.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAMA,
 });
 
 async function getUUID(name) {
